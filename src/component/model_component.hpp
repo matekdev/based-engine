39df8b02a3f518232edd055833bd1612a184bf13
Matthew Zegar <matthewzegar@gmail.com>
2024-05-11 14:19:57 -0700

    Add removal of components

---
+++ src/component/model_component.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class ModelComponent
+{
+public:
+    int TODO = 0;
+};
--- src/ui/inspector_panel.cpp
+++ src/ui/inspector_panel.cpp
@@ -4,6 +4,7 @@
 
 #include "component/info_component.hpp"
 #include "component/transform_component.hpp"
+#include "component/model_component.hpp"
 #include "ui/icon.hpp"
 #include "ui/icon_brands.hpp"
 
@@ -29,23 +30,26 @@ void InspectorPanel::Render()
 
     if (selectedEntity.has_value())
     {
-        auto info = Scene::ActiveScene->Registry.try_get<InfoComponent>(selectedEntity.value());
-        if (info && ImGui::CollapsingHeader(ICON_FA_CIRCLE_INFO " Info", ImGuiTreeNodeFlags_DefaultOpen))
+        if (ImGui::CollapsingHeader(ICON_FA_UNITY " Entity", ImGuiTreeNodeFlags_DefaultOpen))
         {
+            auto info = Scene::ActiveScene->Registry.try_get<InfoComponent>(selectedEntity.value());
             ImGui::PushItemWidth(-1);
             ImGui::InputText("##label", &info->Name);
             ImGui::PopItemWidth();
-        }
 
-        auto transform = Scene::ActiveScene->Registry.try_get<TransformComponent>(selectedEntity.value());
-        if (transform && ImGui::CollapsingHeader(ICON_FA_UNITY " Transform", ImGuiTreeNodeFlags_DefaultOpen))
-        {
+            auto transform = Scene::ActiveScene->Registry.try_get<TransformComponent>(selectedEntity.value());
             auto floatMin = std::numeric_limits<float>::min();
             auto floatMax = -std::numeric_limits<float>::max();
-
             ImGui::DragFloat3("Position", glm::value_ptr(transform->Position), 0.05f, floatMin, floatMax);
             ImGui::DragFloat3("Rotation", glm::value_ptr(transform->Rotation), 0.05f, floatMin, floatMax);
             ImGui::DragFloat3("Scale", glm::value_ptr(transform->Scale), 0.05f, floatMin, floatMax);
+        }
+
+        auto model = Scene::ActiveScene->Registry.try_get<ModelComponent>(selectedEntity.value());
+        if (model && ImGui::CollapsingHeader(ICON_FA_PERSON " Model", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            if (ImGui::Button(ICON_FA_TRASH " Remove", ImVec2(-1, 0)))
+                Scene::ActiveScene->Registry.remove<ModelComponent>(selectedEntity.value());
         }
 
         ImGui::Spacing();
@@ -57,7 +61,7 @@ void InspectorPanel::Render()
 
         if (ImGui::BeginPopup("AddComponent"))
         {
-            DisplayAddComponentEntry<TransformComponent>("Transform Component");
+            DisplayAddComponentEntry<ModelComponent>(ICON_FA_PERSON " Model");
             ImGui::EndPopup();
         }
     }
