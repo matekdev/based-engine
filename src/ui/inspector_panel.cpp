42ec07997685d310adf5e8b7e33d4d8c78d5390a
Matthew Zegar <matthewzegar@gmail.com>
2024-07-09 05:39:29 -0700

    Component header in inspector menu

--- src/ui/inspector_panel.cpp
+++ src/ui/inspector_panel.cpp
@@ -22,24 +22,60 @@ void InspectorPanel::Draw()
         return;
     }
 
-    if (ImGui::CollapsingHeader(ICON_FA_UNITY " Entity", ImGuiTreeNodeFlags_DefaultOpen))
-    {
-        ImGui::BeginGroup();
+    ComponentHeader<TransformComponent>(
+        ICON_FA_UNITY " Entity",
+        [this](TransformComponent *transform)
+        {
+            auto selectedEntity = Scene::ActiveScene->SelectedEntity;
+            auto info = Scene::ActiveScene->Registry.try_get<InfoComponent>(selectedEntity.value());
+            ImGui::PushItemWidth(-1);
+            ImGui::InputText("##label", &info->Name);
+            ImGui::PopItemWidth();
 
-        auto info = Scene::ActiveScene->Registry.try_get<InfoComponent>(selectedEntity.value());
-        ImGui::PushItemWidth(-1);
-        ImGui::InputText("##label", &info->Name);
-        ImGui::PopItemWidth();
+            auto floatMin = std::numeric_limits<float>::min();
+            auto floatMax = -std::numeric_limits<float>::max();
+            ImGui::DragFloat3(ICON_FA_ARROW_UP_RIGHT_FROM_SQUARE " Position", glm::value_ptr(transform->Position), 0.05f, floatMin, floatMax);
+            ImGui::DragFloat3(ICON_FA_ARROWS_ROTATE " Rotation", glm::value_ptr(transform->Rotation), 0.05f, floatMin, floatMax);
+            ImGui::DragFloat3(ICON_FA_MAGNIFYING_GLASS_PLUS " Scale", glm::value_ptr(transform->Scale), 0.05f, floatMin, floatMax);
+        },
+        false);
 
-        auto transform = Scene::ActiveScene->Registry.try_get<TransformComponent>(selectedEntity.value());
-        auto floatMin = std::numeric_limits<float>::min();
-        auto floatMax = -std::numeric_limits<float>::max();
-        ImGui::DragFloat3(ICON_FA_ARROW_UP_RIGHT_FROM_SQUARE " Position", glm::value_ptr(transform->Position), 0.05f, floatMin, floatMax);
-        ImGui::DragFloat3(ICON_FA_ARROWS_ROTATE " Rotation", glm::value_ptr(transform->Rotation), 0.05f, floatMin, floatMax);
-        ImGui::DragFloat3(ICON_FA_MAGNIFYING_GLASS_PLUS " Scale", glm::value_ptr(transform->Scale), 0.05f, floatMin, floatMax);
+    ComponentHeader<ModelComponent>(
+        ICON_FA_PERSON " Model",
+        [this](ModelComponent *model) {
 
-        ImGui::EndGroup();
-    }
+        });
 
     ImGui::End();
+}
+
+template <typename T>
+void InspectorPanel::ComponentHeader(const std::string &name, const std::function<void(T *)> &options, const bool &canDelete)
+{
+    auto flags = ImGuiTreeNodeFlags_AllowOverlap | ImGuiTreeNodeFlags_DefaultOpen;
+    auto selectedEntity = Scene::ActiveScene->SelectedEntity;
+
+    auto component = Scene::ActiveScene->Registry.try_get<T>(selectedEntity.value());
+    if (component)
+    {
+        auto info = Scene::ActiveScene->Registry.try_get<InfoComponent>(selectedEntity.value());
+        auto isOpen = ImGui::CollapsingHeader(name.c_str(), flags);
+
+        ImGui::PushID(info->GetId());
+
+        if (canDelete)
+        {
+            ImGui::SameLine();
+            ImGui::SetCursorPosX(ImGui::GetWindowWidth() - ImGui::GetStyle().ItemSpacing.x - ImGui::GetFrameHeight());
+            ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 18.0f);
+            if (ImGui::Button(ICON_FA_XMARK))
+                Scene::ActiveScene->Registry.remove<T>(Scene::ActiveScene->SelectedEntity.value());
+            ImGui::PopStyleVar();
+        }
+
+        if (isOpen)
+            options(component);
+
+        ImGui::PopID();
+    }
 }
--- src/ui/inspector_panel.hpp
+++ src/ui/inspector_panel.hpp
@@ -2,7 +2,15 @@
 
 #include "panel.hpp"
 
+#include <functional>
+#include <iostream>
+
 class InspectorPanel : public Panel
 {
+public:
     void Draw() override;
+
+private:
+    template <typename T>
+    void ComponentHeader(const std::string &name, const std::function<void(T *)> &options, const bool &canDelete = true);
 };
