bfa130075683494191b68359fa22d402302560a8
Matthew Zegar <matthewzegar@gmail.com>
2024-10-04 17:22:34 -0700

    Init physx with an example

--- src/scene.cpp
+++ src/scene.cpp
@@ -13,12 +13,29 @@ Scene::Scene() : _renderTarget(RenderTarget()),
 {
     ActiveScene = this;
 
+    InitializePhysics();
+
     // TODO: Remove debug.
     auto ent = Registry.create();
     Registry.emplace<InfoComponent>(ent, ent);
     Registry.emplace<TransformComponent>(ent, ent);
     auto &model = Registry.emplace<ModelComponent>(ent, ent);
     model.LoadModel("models\\dev_orange_cube\\dev_orange_cube.obj");
+}
+
+Scene::~Scene()
+{
+    PX_RELEASE(_pxScene);
+    PX_RELEASE(_pxDispatcher);
+    PX_RELEASE(_pxPhysics);
+    if (_pxPvd)
+    {
+        auto *transport = _pxPvd->getTransport();
+        PX_RELEASE(_pxPvd);
+        PX_RELEASE(transport);
+    }
+
+    PX_RELEASE(_pxFoundation);
 }
 
 ID3D11ShaderResourceView *Scene::GetShaderResourceView()
@@ -69,6 +86,9 @@ void Scene::Render()
     }
 
     _camera.Update();
+
+    _pxScene->simulate(1.0f / 60.0f);
+    _pxScene->fetchResults(true);
 }
 
 void Scene::CalculateDeltaTime()
@@ -76,4 +96,57 @@ void Scene::CalculateDeltaTime()
     auto currentFrame = glfwGetTime();
     _deltaTime = currentFrame - _previousFrameTime;
     _previousFrameTime = currentFrame;
+}
+
+// TODO:: remove debug method
+void Scene::createStack(const physx::PxTransform &t, physx::PxU32 size, physx::PxReal halfExtent)
+{
+    auto *shape = _pxPhysics->createShape(physx::PxBoxGeometry(halfExtent, halfExtent, halfExtent), *_pxMaterial);
+    for (physx::PxU32 i = 0; i < size; i++)
+    {
+        for (physx::PxU32 j = 0; j < size - i; j++)
+        {
+            physx::PxTransform localTm(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 + 1), 0) * halfExtent);
+            physx::PxRigidDynamic *body = _pxPhysics->createRigidDynamic(t.transform(localTm));
+            body->attachShape(*shape);
+            physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+            _pxScene->addActor(*body);
+        }
+    }
+    shape->release();
+}
+
+void Scene::InitializePhysics()
+{
+    _pxFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, _pxAllocator, _pxErrorCallback);
+
+    _pxPvd = physx::PxCreatePvd(*_pxFoundation);
+    auto *transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+    _pxPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+    _pxPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *_pxFoundation, physx::PxTolerancesScale(1.0f, 10.0f), true, _pxPvd);
+
+    auto sceneDesc = physx::PxSceneDesc(_pxPhysics->getTolerancesScale());
+    sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f); // TODO: Maybe some way to change this.
+    _pxDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+    sceneDesc.cpuDispatcher = _pxDispatcher;
+    sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+    _pxScene = _pxPhysics->createScene(sceneDesc);
+
+    auto *pvdClient = _pxScene->getScenePvdClient();
+    if (pvdClient)
+    {
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+    }
+
+    // TODO: Remove this, we should create the ground plane somewhere else.
+    _pxMaterial = _pxPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+    auto *groundPlane = physx::PxCreatePlane(*_pxPhysics, physx::PxPlane(0, 1, 0, 0), *_pxMaterial);
+    _pxScene->addActor(*groundPlane);
+
+    for (physx::PxU32 i = 0; i < 5; i++)
+        createStack(physx::PxTransform(physx::PxVec3(0, 0, 0)), 10, 2.0f);
 }
--- src/scene.hpp
+++ src/scene.hpp
@@ -6,12 +6,15 @@
 #include "render/pixel_shader.hpp"
 #include "render/render_target.hpp"
 
+#include <PxPhysicsAPI.h>
+
 #include <optional>
 
 class Scene
 {
 public:
     Scene();
+    ~Scene();
 
     // Ideally this shouldn't be static and public... but I'm here to do graphics shit.
     static inline Scene *ActiveScene;
@@ -27,6 +30,8 @@ public:
     void Render();
 
 private:
+    const std::string LOCAL_HOST = "127.0.0.1";
+
     Camera _camera;
 
     VertexShader _modelVertexShader;
@@ -34,8 +39,20 @@ private:
 
     RenderTarget _renderTarget;
 
+    physx::PxDefaultAllocator _pxAllocator;
+    physx::PxDefaultErrorCallback _pxErrorCallback;
+    physx::PxFoundation *_pxFoundation;
+    physx::PxPhysics *_pxPhysics;
+    physx::PxDefaultCpuDispatcher *_pxDispatcher;
+    physx::PxScene *_pxScene;
+    physx::PxPvd *_pxPvd;
+
+    physx::PxMaterial *_pxMaterial;
+    void createStack(const physx::PxTransform &t, physx::PxU32 size, physx::PxReal halfExtent);
+
     float _previousFrameTime;
     float _deltaTime;
 
     void CalculateDeltaTime();
+    void InitializePhysics();
 };
